d7fdd4b80ec68aac71d74ee6f0b5d257a802f0fe
Alexei Boronine <alexei@boronine.com>
2025-09-01 13:14:58 +0200

    wip

--- build.sh
+++ build.sh
@@ -1,27 +1,27 @@
 
 #!/usr/bin/env sh
 
-# set -e
+set -e
 
-# if [[ -z "$PORTABLEVPN_HOSTNAME" ]]; then
-#     echo "Must provide PORTABLEVPN_HOSTNAME in environment"
-#     exit 1
-# fi
+if [[ -z "$PORTABLEVPN_HOSTNAME" ]]; then
+    echo "Must provide PORTABLEVPN_HOSTNAME in environment"
+    exit 1
+fi
 
-# rm -rf build dist
-# mkdir dist
+rm -rf build dist
+mkdir dist
 
-# # Build and print logs
-# docker build --progress=plain .
-# # Extract SHA256 of the image
-# IMAGE_ID=$(docker build -q .)
-# docker save "$IMAGE_ID" > "dist/portablevpn.tar"
-# docker image rm "$NAME" 2> /dev/null
-# CONTAINER_ID=$(docker create $IMAGE_ID)
-# docker cp "$CONTAINER_ID:/openvpn-ca/pki" build
-# docker rm "$CONTAINER_ID" 2> /dev/null
+# Build and print logs
+docker build --progress=plain .
+# Extract SHA256 of the image
+IMAGE_ID=$(docker build -q .)
+docker save "$IMAGE_ID" > "dist/portablevpn.tar"
+docker image rm "$NAME" 2> /dev/null
+CONTAINER_ID=$(docker create $IMAGE_ID)
+docker cp "$CONTAINER_ID:/openvpn-ca/pki" build
+docker rm "$CONTAINER_ID" 2> /dev/null
 
-for i in $(seq 1 128); do
+for i in $(seq 1 16); do
     {
         echo "client"
         echo "nobind"
@@ -40,5 +40,5 @@ for i in $(seq 1 128); do
         echo "key-direction 1"
         echo "redirect-gateway def1"
 
-    } > "dist/atlas2-$i.ovpn"
+    } > "dist/client$i.ovpn"
 done
